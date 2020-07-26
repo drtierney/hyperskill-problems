66638d7e0262b78dc903f9b9a8d7f7f9243b54b3
David Tierney <d.r.tierney@hotmail.co.uk>
2020-07-26 19:20:35 +0100

    Adding new completed problems

---
+++ Swaps/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = Arrays.asList(scanner.nextLine()
+                .split(" ")).stream()
+                .mapToInt(Integer::parseInt).toArray();
+
+        System.out.println(bubbleSortSwapCount(numbers));
+    }
+
+    public static int bubbleSortSwapCount(int[] array) {
+        int count = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
