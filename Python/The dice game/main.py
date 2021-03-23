e4723d4d2accbe541dfe4fbf3d9ae4b46376ecb3
drtierney <d.r.tierney@hotmail.co.uk>
2021-03-23 17:59:15 +0000

    Added new completed problems

---
+++ Python/The dice game/main.py
@@ -0,0 +1,8 @@
+import random
+
+
+# this line is needed for us to check the results, don't modify it please
+random.seed(int(input()))
+
+# use a function from the random module in the next line
+print(random.randint(1, 6))
---
+++ Python/Voldemort/main.py
@@ -0,0 +1,8 @@
+import random
+
+
+# work with this variable
+n = int(input())
+
+random.seed(n)
+print(random.choice("Voldemort"))
