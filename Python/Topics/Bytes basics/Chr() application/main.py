da0e6368adab42e2a41f78196831f3a8fa81c229
drtierney <d.r.tierney@hotmail.co.uk>
2021-04-11 17:54:39 +0100

    Added new topic problems - Bytes basics

---
+++ Python/Topics/Bytes basics/Chr() application/main.py
@@ -0,0 +1,3 @@
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+
+print(chr(a) + chr(b) + chr(c) + chr(d))
---
+++ Python/Topics/Bytes basics/Printable/main.py
@@ -0,0 +1,2 @@
+a = int(input())
+print(chr(a) if 32 <= a <= 126 else False)
