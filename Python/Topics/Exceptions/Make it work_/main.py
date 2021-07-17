0a926f87c5851528109a86e6f1ac1b60ce320ffb
drtierney <d.r.tierney@hotmail.co.uk>
2021-07-17 17:28:51 +0100

    Added new topic problems - Exceptions

---
+++ Python/Topics/Exceptions/Make it work_/main.py
@@ -0,0 +1,9 @@
+first = int(input())
+second = int(input())
+
+if first > second:
+    print("The first one wins")
+elif second > first:
+    print("The second one wins")
+else:
+    print("Draw")
---
+++ Python/Topics/Exceptions/The best choice/main.py
@@ -0,0 +1,7 @@
+print("What would I watch tonight?")
+
+# declare the variable before using it
+fave_tv_show = "Scrubs"
+
+# any answer will do, since heart knows no law
+my_answer = fave_tv_show
