e513e329f33f100b1c3327c9b39b5b2db39c1f7f
drtierney <d.r.tierney@hotmail.co.uk>
2021-07-19 16:26:30 +0100

    Added new topic problems - Regexp quantifiers

---
+++ Python/Topics/Regexp quantifiers/Valid password/main.py
@@ -0,0 +1,5 @@
+import re
+
+
+# put your regex in the variable template
+template = r"^[A-Za-z0-9_-]{5,}$"
---
+++ Python/Topics/Regexp quantifiers/Vehicle registration plate/main.py
@@ -0,0 +1,5 @@
+import re
+
+
+# put your regex in the variable template
+template = r'^[A-Z]{2}\s?\d{2}\s?[A-Z]{2}\s?\d{4}$'
